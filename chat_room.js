4226294fa150e38ff668d885e522fe5953790536
dvhi <131928785+dvhi@users.noreply.github.com>
2023-07-31 18:32:22 -0700

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBPzzIan5xMQmDpp45DHp3wUW-nfg7FzDg",
+    authDomain: "let-s-chat-web-app-ef9b4.firebaseapp.com",
+    databaseURL: "https://let-s-chat-web-app-ef9b4-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-web-app-ef9b4",
+    storageBucket: "let-s-chat-web-app-ef9b4.appspot.com",
+    messagingSenderId: "860728589299",
+    appId: "1:860728589299:web:2d790f13c1592212b67086"
+  };
+firebase.initializeApp(firebaseConfig);
